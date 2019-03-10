80bc4c652f53c879839768d8adb4a62ca987878c
NatanDC <natandc740@gmail.com>
2019-03-10 17:50:24 -0600

    third commit

--- src/index.js
+++ src/index.js
@@ -11,18 +11,35 @@ class DogeMeme extends React.Component{
       super(props);
       
       this.state = {
-         top: 'EASY PEASY',
-         bottom: 'LEMON SQUEEZY'
+         top: 'Top',
+         bottom: 'Bottom'
       }
+      this.updateState = this.updateState.bind(this);
+
+      this.updateState2 = this.updateState2.bind(this);
+
 
 
    };
+   updateState(e) {
+      this.setState({top: e.target.value});
+   }
 
+   updateState2(c) {
+   		this.setState({bottom: c.target.value});
+   }
 
    render() {
       return (
          <div >
+         	<div>
+         	<input type = "text" value = {this.state.top} 
+               onChange = {this.updateState} />
+            <input type = "text" value = {this.state.bottom}
+            	onChange = {this.updateState2} />
+         	</div>
           
+
             <div className= "bg">
             	<div class= "top myfont" >
             	{this.state.top}
